24da5fd0b55bfe0c58e9e7915575f8554306c762
陆晓阳 <luxy25@163.com>
2019-01-08 10:26:15 +0800

    提交配置

---
+++ config/base.php
@@ -0,0 +1,104 @@
+<?php return array (
+  'site_name' => 'OneIndex',
+  'password' => 'oneindex',
+  'style' => 'material',
+  'onedrive_root' => '',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'root_path' => '?',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'client_secret' => 'etghwjBINI17*[~pURO104^',
+  'client_id' => '4e3bc86f-bb1b-400d-8c14-cf8b3ce6ef58',
+  'redirect_uri' => 'https://ju.tn/',
+  'refresh_token' => 'OAQABAAAAAAC5una0EUFgTIF8ElaxtWjTa4SObIE2EfbIx3CvcAz7PfAatsSl6NU_GkrUAfkdplxydijxDqs7hZR7sA8U7ga9Rz_SIGjeB9Lk_N95nNOaSBlojJsOVqNRSmQkaludXuqMKFsPtkmye6YEf3kpl0w2yEZ3HRzbRVQjSvNJ5ZOgNVMaDkfcFcANj_j4kLkeZu4ChsxAVV11VUjilrrMftYbrPyDgRCHmaYuaonn1bSFd4ol68AGjMkmtV7jk_zFvvaWM1EsMvx4pozf3WejTP4eai5B3H7UGMkxYEph23JwBM68Yne-WEVnrAKDjPOXouIFtY3-TRYoUWbbcaUWZt9dmDgdZhTg9tKrA66nOyOku5H0CuIL1ZP8LwmYnLnh0WGx4fOHCKjbBD3EFGjK1vvVFGJspHlk42_vUVCuxeoPvNLvHv7m8b_Yb4OjLyeIrb-lPMTCCPXBQTHAvkRRSj40Mi7VQ0UWDGQKelYBnAgFpSs5hLtrOO2_f9y8sORYnugA7vJRjHQ2pYJDrRJkidtqKur9-mXXWO_tARrkh3bs8Xev7_Qbt_RDBCq-SeY684oLNqJFWc3JjOz_Q5CV8ytrYcnhlOWCGDVgzRcuRSUJEOyFggAw0SN3E6ppybboipOGWnBbmQoF9eQVAlWOlZ2-NeY84vRnY18ZPmhcSPGoPG6IFbidca0gY1rIgod5JFpJ7qQk695xeRbi6ma6O465lAqH9IVZcdzeC-24qdbe61mJ0KPG50yeQ_xwQi5DS8UAlKjpfcushhVc9AHHKWUaydB0a47fnenv_bnTiIgeByAA',
+);
